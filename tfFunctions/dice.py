98e6e79609120a7b48d0654c503f17fd2d9067c4
snow_leps <mattdns100689@gmail.com>
2016-11-28 14:32:19 +0000

    Dice improved to generalize to multiple thresholds through diceROC

--- tfFunctions/dice.py
+++ tfFunctions/dice.py
@@ -1,37 +1,55 @@
 import tensorflow as tf
+import numpy as np
 
-def dice(yPred,yTruth,thresh):
+'''
+Only suitable for 3-dimensional tensors at the moment.
+'''
+
+def dice(yPred,yTruth,threshold=0.5):
     smooth = tf.constant(1.0)
-    threshold = tf.constant(thresh)
+    threshold = tf.constant(threshold)
     yPredThresh = tf.to_float(tf.greater_equal(yPred,threshold))
     mul = tf.mul(yPredThresh,yTruth)
-    intersection = 2*tf.reduce_sum(mul) + smooth
-    union = tf.reduce_sum(yPredThresh) + tf.reduce_sum(yTruth) + smooth
+    intersection = 2*tf.reduce_sum(mul, [0,1,2]) + smooth
+    union = tf.reduce_sum(yPredThresh, [0,1,2]) + tf.reduce_sum(yTruth, [0,1,2]) + smooth
     dice = intersection/union
     return dice, yPredThresh
+
+def diceROC(yPred,yTruth,thresholds=np.linspace(0.1,0.9,20)):
+    thresholds = thresholds.astype(np.float32)
+    nThreshs = thresholds.size
+    nDims = len(yPred.get_shape())
+    yPred = tf.expand_dims(yPred,nDims)
+    yTruth = tf.expand_dims(yTruth,nDims)
+    dims = np.ones(nDims+1).astype(np.uint32)
+    dims[-1] = nThreshs
+    yPredT = tf.tile(yPred,dims)
+    yTruthT = tf.tile(yTruth,dims)
+    diceScores, _ = dice(yPred=yPred, yTruth=yTruth, threshold = thresholds)
+    return diceScores
 
 if __name__ == "__main__":
     import ipdb
     with tf.Session() as sess:
 
+        nThreshs = 20
         thresh = 0.5
         print("Dice example")
-        yPred = tf.constant([0.1,0.9,0.7,0.3,0.1,0.1,0.9,0.9,0.1],shape=[3,3])
-        yTruth = tf.constant([0.0,1.0,1.0,0.0,0.0,0.0,1.0,1.0,1.0],shape=[3,3])
-        diceScore, yPredThresh= dice(yPred=yPred,yTruth=yTruth,thresh= thresh)
+        yPred = tf.constant([0.1,0.9,0.7,0.3,0.1,0.1,0.9,0.9,0.1,
+                             0.1,0.9,0.7,0.3,0.1,0.1,0.9,0.9,0.1,
+                             0.1,0.9,0.7,0.3,0.1,0.1,0.9,0.9,0.1],shape=[3,3,3])
+        yTruth = tf.constant([0.0,1.0,1.0,0.0,0.0,0.0,1.0,1.0,1.0,
+                              0.0,1.0,1.0,0.0,0.0,0.0,1.0,1.0,1.0,
+                              0.0,1.0,1.0,0.0,0.0,0.0,1.0,1.0,1.0],shape=[3,3,3])
 
-        diceScore_ , yPredThresh_ , yPred_, yTruth_ = sess.run([diceScore,yPredThresh,yPred, yTruth])
+        threshs = np.linspace(0.1,0.9,nThreshs)
+        diceScore, _= dice(yPred=yPred,yTruth=yTruth,threshold=thresh)
+        diceScoreROC = diceROC(yPred=yPred,yTruth=yTruth,thresholds=threshs)
 
+        diceScore_ = diceScore.eval()
+        diceScoreROC_ , yPred_, yTruth_ = sess.run([diceScoreROC, yPred, yTruth])
 
-        print("\nTruth = ")
-        print(yTruth_)
-
-        print("\nPrediction = ")
-        print(yPred_)
-
-
-        print("\nPrediction thresholded at {0} = \n".format(thresh))
-        print(yPredThresh_)
-
-        print("\nScore = {0}".format(diceScore_))
         ipdb.set_trace()
+
+        print("\nScore = {0}".format(diceScoreROC_))
+
