f8eed96511457656886c7048fd30a77914e06055
snow_leps <mattdns100689@gmail.com>
2017-01-09 16:51:58 +0000

    batch norm update

--- tfFunctions/batchNorm.py
+++ tfFunctions/batchNorm.py
@@ -1,4 +1,5 @@
 import tensorflow as tf  
+from tensorflow.python.training import moving_averages
 
 def batch_norm(x,training,name="bn"):
     """Batch normalization."""
@@ -13,7 +14,8 @@ def batch_norm(x,training,name="bn"):
           initializer=tf.constant_initializer(1.0, tf.float32))
 
       if training == True:
-        mean, variance = tf.nn.moments(x, [0, 1, 2], name='moments')
+        dim = [0,1,2]
+        mean, variance = tf.nn.moments(x, dim, name='moments')
 
         moving_mean = tf.get_variable(
             'moving_mean', params_shape, tf.float32,
@@ -24,10 +26,8 @@ def batch_norm(x,training,name="bn"):
             initializer=tf.constant_initializer(1.0, tf.float32),
             trainable=False)
 
-        self._extra_train_ops.append(moving_averages.assign_moving_average(
-            moving_mean, mean, 0.9))
-        self._extra_train_ops.append(moving_averages.assign_moving_average(
-            moving_variance, variance, 0.9))
+        moving_averages.assign_moving_average( moving_mean, mean, 0.9)
+        moving_averages.assign_moving_average( moving_variance, variance, 0.9)
       else:
         mean = tf.get_variable(
             'moving_mean', params_shape, tf.float32,
@@ -37,10 +37,9 @@ def batch_norm(x,training,name="bn"):
             'moving_variance', params_shape, tf.float32,
             initializer=tf.constant_initializer(1.0, tf.float32),
             trainable=False)
-        tf.histogram_summary(mean.op.name, mean)
-        tf.histogram_summary(variance.op.name, variance)
       # elipson used to be 1e-5. Maybe 0.001 solves NaN problem in deeper net.
       y = tf.nn.batch_normalization(
           x, mean, variance, beta, gamma, 0.001)
       y.set_shape(x.get_shape())
       return y
+
