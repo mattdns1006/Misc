614886128e08135a9f696160ae68aa4d3bdf0864
snow_leps <mattdns100689@gmail.com>
2017-02-02 17:53:17 +0000

    updates n shiz

--- weightInitialization/conv.py
+++ weightInitialization/conv.py
@@ -1,176 +1,212 @@
-
-'''
-A Convolutional Network implementation example using TensorFlow library.
-This example is using the MNIST database of handwritten digits
-(http://yann.lecun.com/exdb/mnist/)
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-'''
-
 from __future__ import print_function
 import tensorflow.contrib.layers as layers
 import tensorflow as tf
-#bn = layers.batch_norm
 import sys
 sys.path.append("/home/msmith/misc/tfFunctions")
-from batchNorm import batch_norm as bn
 import numpy as np
 import pdb
-#np.random.seed(1006)
+np.random.seed(1006)
 
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+def W(shape,weightInit,scale=0.05):
+    if len(shape) == 2:
+        fan_in = shape[0]
+        fan_out = shape[1]
+    elif len(shape) == 4:
+        fw, fh, nIn, nOut = shape
+        fan_in = fw*fh*nIn # Number of weights for one output neuron in nOut
+        fan_out = nOut #  
+    else:
+        raise ValueError, "Not a valid shape"
 
-# Parameters
-learning_rate = 0.001
-training_iters = 200000
-batch_size = 128
-display_step = 10
+    if weightInit == "uniform":
+        init = tf.random_uniform(shape,minval=-scale,maxval=scale)
 
-# Network Parameters
-n_input = 784 # MNIST data input (img shape: 28*28)
-n_classes = 10 # MNIST total classes (0-9 digits)
-dropout = 0.75 # Dropout, probability to keep units
+    elif weightInit == "normal":
+        init = tf.random_normal(shape,mean=0,stddev=scale)
 
-# tf Graph input
-x = tf.placeholder(tf.float32, [None, n_input])
-y = tf.placeholder(tf.float32, [None, n_classes])
-keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
+    elif weightInit == "lecun_uniform":
+        scale = np.sqrt(3.0/(fan_in))
+        init = tf.random_uniform(shape,minval=-scale,maxval=scale)
+
+    elif weightInit == "glorot_normal":
+        scale = np.sqrt(2.0/(fan_in+fan_out))
+        init = tf.random_normal(shape,mean=0,stddev=scale)
+
+    elif weightInit == "glorot_uniform":
+        scale = np.sqrt(6.0/(fan_in+fan_out))
+        init = tf.random_uniform(shape,minval=-scale,maxval=scale)
+
+    elif weightInit == "zeros":
+        init = tf.zeros(shape)
+    else:
+        raise ValueError, "{0} not a valid weight intializer.".format(weightInit)
+
+    return tf.Variable(init)
+
 
 def variable_summaries(var):
     """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
     with tf.name_scope('summaries'):
         mean = tf.reduce_mean(var)
         tf.summary.scalar('mean', mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
-        tf.summary.histogram('histogram', var)
 
-# Create some wrappers for simplicity
+
 def conv2d(x, W, b, strides):
     with tf.name_scope("convolution"):
-        # Conv2D wrapper, with bias and relu activation
         with tf.name_scope("w"):
             x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
         with tf.name_scope("b"):
             x = tf.nn.bias_add(x, b)
-
         return tf.nn.relu(x)
 
 def maxpool2d(x, k=2):
-    # MaxPool2D wrapper
     with tf.name_scope("mp"):
         return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
                               padding='SAME')
 
-# Create model
-def conv_net(x, weights, biases, dropout,batchNorm,training):
+def conv_net(x, weights, biases, dropout):
+    max_outputs = 6 
     # Reshape input picture
-    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+    with tf.name_scope("input"):
+        x = tf.reshape(x, shape=[-1, 28, 28, 1])
+        tf.summary.image("x",x,max_outputs=max_outputs)
 
     # Convolution Layer
     with tf.name_scope("layer1"):
         conv1 = conv2d(x, weights['wc1'], biases['bc1'],strides=1)
-        print("Using batchNorm")
-        conv1 = bn(conv1,is_training=training,name="bn1")
         conv1 = maxpool2d(conv1, k=2)
+
+        with tf.name_scope("f1"):
+            for i in xrange(10):
+                f = tf.split(3,16,conv1)[i]
+                #w1 = tf.split(3,32,weights["wc1"])[0]
+                tf.summary.image("f1_{0}".format(i),f,max_outputs=max_outputs)
+                #tf.image_summary("w1",w1)
+
 
     with tf.name_scope("layer2"):
         conv2 = conv2d(conv1, weights['wc2'], biases['bc2'],strides=1)
-        print("Using batchNorm")
-        conv2 = bn(conv2,is_training=training,name="bn2")
         conv2 = maxpool2d(conv2, k=2)
 
-    with tf.name_scope("dense"):
-        # Fully connected layer
-        # Reshape conv2 output to fit fully connected layer input
-        pdb.set_trace()
-        fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+        with tf.name_scope("f2"):
+            for i in xrange(10):
+                f = tf.split(3,16,conv2)[i]
+                tf.summary.image("f2_{0}".format(i),f,max_outputs=max_outputs)
 
+    with tf.name_scope("layer3"):
+        conv3 = conv2d(conv2, weights['wc3'], biases['bc3'],strides=1)
+        conv3 = maxpool2d(conv3, k=2)
+
+        with tf.name_scope("f3"):
+            for i in xrange(10):
+                f = tf.split(3,16,conv3)[i]
+                tf.summary.image("f3_{0}".format(i),f,max_outputs=max_outputs)
+
+    with tf.name_scope("layer4"):
+        conv4 = conv2d(conv3, weights['wc4'], biases['bc4'],strides=1)
+        conv4 = maxpool2d(conv4, k=2)
+
+        with tf.name_scope("f4"):
+            for i in xrange(10):
+                f = tf.split(3,16,conv4)[i]
+                tf.summary.image("f4_{0}".format(i),f,max_outputs=max_outputs)
+
+
+    with tf.name_scope("dense"):
+        fc1 = tf.reshape(conv4, [-1, weights['wd1'].get_shape().as_list()[0]])
         fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
         fc1 = tf.nn.relu(fc1)
-        # Apply Dropout
         #fc1 = tf.nn.dropout(fc1, dropout)
 
         # Output, class prediction
     with tf.name_scope("out"):
         out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+        outVis = tf.reshape(out,[-1,10,1,1])
+        tf.summary.image("outVis_{0}".format(i),outVis,max_outputs=max_outputs)
     return out
 
-# Store layers weight & bias
-weights = {
-    # 5x5 conv, 1 input, 32 outputs
-    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-    # 5x5 conv, 32 inputs, 64 outputs
-    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-    # fully connected, 7*7*64 inputs, 1024 outputs
-    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
-    # 1024 inputs, 10 outputs (class prediction)
-    'out': tf.Variable(tf.random_normal([1024, n_classes]))
-}
 
-biases = {
-    'bc1': tf.Variable(tf.random_normal([32])),
-    'bc2': tf.Variable(tf.random_normal([64])),
-    'bd1': tf.Variable(tf.random_normal([1024])),
-    'out': tf.Variable(tf.random_normal([n_classes]))
-}
+if __name__ == "__main__":
+    # Import MNIST data
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-# Construct model
-training = tf.placeholder(tf.bool)
+    # Parameters
+    learning_rate = 0.001
+    training_iters = 60000
+    batch_size = 128
+    display_step = 10
 
-batchNorm = 1
-if batchNorm == 1:
-    path = "bn/"
-else:
-    path = ""
-pred = conv_net(x, weights, biases, keep_prob,batchNorm=batchNorm,training=training)
+    # Network Parameters
+    n_input = 784 # MNIST data input (img shape: 28*28)
+    n_classes = 10 # MNIST total classes (0-9 digits)
+    dropout = 0.75 # Dropout, probability to keep units
 
-# Define loss and optimizer
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+    # tf Graph input
 
-# Evaluate model
-correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-variable_summaries(accuracy)
+    # Construct model
+    for weightInit in ["uniform","normal","lecun_uniform","glorot_uniform","glorot_normal","zeros"][:1]:
+        tf.reset_default_graph()
+        weights = {
+            'wc1': W([5, 5, 1, 16],weightInit),
+            'wc2': W([5, 5, 16, 16],weightInit),
+            'wc3': W([5, 5, 16, 16],weightInit),
+            'wc4': W([3, 3, 16, 16],weightInit),
+            'wd1': W([2*2*16, 32],weightInit),
+            'out': W([32, n_classes],weightInit)
+        }
 
-# Merged
-merged = tf.summary.merge_all()
+        biases = {
+            'bc1': tf.Variable(tf.constant(0.0,shape=[16])),
+            'bc2': tf.Variable(tf.constant(0.0,shape=[16])),
+            'bc3': tf.Variable(tf.constant(0.0,shape=[16])),
+            'bc4': tf.Variable(tf.constant(0.0,shape=[16])),
+            'bd1': tf.Variable(tf.constant(0.0,shape=[32])),
+            'out': tf.Variable(tf.constant(0.0,shape=[n_classes]))
+        }
+        x = tf.placeholder(tf.float32, [None, n_input])
+        y = tf.placeholder(tf.float32, [None, n_classes])
+        keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
+        training = tf.placeholder(tf.bool)
 
-# Initializing the variables
-init = tf.initialize_all_variables()
+        pred = conv_net(x, weights, biases, keep_prob)
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-# Launch the graph
-with tf.Session() as sess:
+        # Evaluate model
+        correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+        variable_summaries(accuracy)
 
-    train_writer = tf.summary.FileWriter('train/{0}'.format(path),sess.graph)
-    test_writer = tf.summary.FileWriter('test/{0}'.format(path),sess.graph)
-    sess.run(init)
-    step = 1
-    # Keep training until reach max iterations
-    while step * batch_size < training_iters:
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        # Run optimization op (backprop)
-        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y, keep_prob: dropout,training:True})
-        if step % display_step == 0:
-            # Calculate batch loss and accuracy
-            loss, acc, summary = sess.run([cost, accuracy,merged], feed_dict={x: batch_x, y: batch_y, keep_prob: 1.,training:False})
-            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + ", Training Accuracy= " + "{:.5f}".format(acc))
-            train_writer.add_summary(summary,step)
+        merged = tf.summary.merge_all()
+        init = tf.initialize_all_variables()
+        path = "conv/"
 
-            #Test
-            acc, summary = sess.run([accuracy,merged], feed_dict={x: mnist.test.images[:256], y: mnist.test.labels[:256], keep_prob: 1.0,training:False})
-            test_writer.add_summary(summary,step)
-            print("Testing Accuracy:", acc)
-        step += 1
+        with tf.Session() as sess:
 
-    print("Optimization Finished!")
+            #train_writer = tf.summary.FileWriter('{0}/{1}/train'.format(path,weightInit),sess.graph)
+            test_writer = tf.summary.FileWriter('{0}/{1}/test'.format(path,weightInit),sess.graph)
+            sess.run(init)
+            step = 1
+            # Keep training until reach max iterations
+            while step * batch_size < training_iters:
+                batch_x, batch_y = mnist.train.next_batch(batch_size)
+                # Run optimization op (backprop)
+                sess.run(optimizer, feed_dict={x: batch_x, y: batch_y, keep_prob: dropout,training:True})
+                if step % display_step == 0:
+                    # Calculate batch loss and accuracy
+                    #loss, acc, summary = sess.run([cost, accuracy,merged], feed_dict={x: batch_x, y: batch_y, keep_prob: 1.,training:False})
+                    #print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + ", Training Accuracy= " + "{:.5f}".format(acc))
+                    #train_writer.add_summary(summary,step)
 
-    # Calculate accuracy for 256 mnist test images
+                    #Test
+                    acc, summary = sess.run([accuracy,merged], feed_dict={x: mnist.test.images[:256], y: mnist.test.labels[:256], keep_prob: 1.0,training:False})
+                    test_writer.add_summary(summary,step)
+                    print("Testing Accuracy:", acc)
+                step += 1
+
+            print("Optimization Finished!")
+
+        # Calculate accuracy for 256 mnist test images
 
 
